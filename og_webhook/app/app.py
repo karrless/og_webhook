6d83a5d5f3b1ae921aae9e4776817e0ede1a2e48
Константин Гейдаров <konstantin.geydarov@gmail.com>
2025-07-18 23:32:52 +0300

    fix

--- og_webhook/app/app.py
+++ og_webhook/app/app.py
@@ -44,21 +44,23 @@ async def vk_callback(request: Request):
     if data["type"] == "message_new":
         message = data["object"]["message"]
         if message["peer_id"] not in chat_ids:
+            if message.get("action"):
+                if message['action']['member_id'] < 0:
+                    with s_factory() as session:
+                        logger.info(f"Бота добавили в {message['peer_id']}")
+                        await write_new_chat(session, message['peer_id'], chat_ids)
+                        session.commit()
             return PlainTextResponse("ok")
 
         if message.get("action"):
             with s_factory() as session:
-                if message['action']['member_id'] < 0:
-                    logger.info(f"Бота добавили в {message['peer_id']}")
-                    await write_new_chat(session, message['peer_id'], chat_ids)
-                else:       
-                    logger.debug(f"Действие с пользователем {message['from_id']}")
-                    await check_user(session, message["from_id"], users)
+                logger.debug(f"Действие с пользователем {message['from_id']}")
+                await check_user(session, message["from_id"], users)
                 
                 session.commit()
-                return PlainTextResponse("ok")
+            return PlainTextResponse("ok")
 
-        logger.debug(f"{message['from_id']}: {message['text']}\n")
+        logger.debug(f"{message['peer_id']}-{message['from_id']}: {message['text']}\n")
 
         sticker_id = None
         sticker_url = None
